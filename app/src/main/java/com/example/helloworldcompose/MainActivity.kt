83909becede15eef135998b65db36ac1c496d002
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-05-31 15:48:34 -0300

    chore: add image and news texts

--- app/src/main/java/com/example/helloworldcompose/MainActivity.kt
+++ app/src/main/java/com/example/helloworldcompose/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.helloworldcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworldcompose.ui.theme.HelloWorldComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +23,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloWorldComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("World")
+                    NewsStory()
                 }
             }
         }
@@ -25,14 +32,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.header_test),
+            contentDescription = null,
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Divider(
+            color = Color.Black,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Column {
+            Text("A day in Shark Fin Cove")
+            Text("Davenport, California")
+            Text("December 2018")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultNewsStory() {
     HelloWorldComposeTheme {
-        Greeting("World")
+        NewsStory()
     }
 }
